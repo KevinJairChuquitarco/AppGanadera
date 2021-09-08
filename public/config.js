4e47e31d9a8235173782881659179cf08c334a2c
MEstefania <mishell08cv@gmail.com>
2021-09-07 20:58:51 -0500

    Conexión BBDD

---
+++ public/config.js
@@ -0,0 +1,11 @@
+ // config for your database
+ var config = {
+    user: 'sa',
+    password: 'mISS hACK',
+    server: 'localhost', 
+    database: 'AppGanadera', 
+    options: { 
+        trustServerCertificate: true,
+    } 
+};
+module.exports = config;
