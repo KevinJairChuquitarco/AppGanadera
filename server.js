6c702a71bddc4c40f891981ce29eacfe92f80471
KevinJairChuquitarco <kevinchuquitarco11@gmail.com>
2021-09-10 16:31:49 -0500

    server Listo

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+const funciones = require('./funciones');
+
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('static'));
 
 const port = process.env.PORT || 3000 ; 
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine','hbs');
-
 app.get('/',(req,res)=>{
     res.render('index');
 });
 
-app.get('/add',(req,res)=>{
+app.get('/registrarUsuario',(req,res)=>{
     res.render('registrarUsuario');
 });
 
 app.post('/registrarUsuario',(req,res)=>{
-    console.log(req.body);
-    res.send('ok');
+    let datos = req.body;
+    funciones.RegistrarUsuario(datos.nombre,'2000/12/01',datos.telefono,datos.email,datos.password);
+    res.send("Listo");
+});
+
+app.get('/inicioSesion',(req,res)=>{
+    res.render('inicioSesion');
 });
 
 app.listen(port,()=>{
